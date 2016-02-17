e8659d1afd51706fb7222a9ef465d78cc9dc3f4e
TheUnCola <Steven.sell1@gmail.com>
2016-02-17 18:41:18 -0500

    Play

--- src/play/Play.java
+++ src/play/Play.java
@@ -1,5 +1,20 @@
 package play;
 
-public class Play {
+import java.util.Scanner;
 
+public class Play {
+	public static void main(String[] args) {
+		menu();
+	}
+	
+	public static void menu() {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Would you like to play?");
+		String menu = scan.next();
+		if(menu.equals("Yes")) start();
+	}
+	
+	public static void start() {
+		System.out.println("The Game Has Begun");
+	}
 }
